1afded74f4eb1d6b3d0367d083694197d6786d8f
A13RD <alejandro.rdiaz.0221@gmail.com>
2026-05-04 23:17:25 -0500

    KongImplementacion

---
+++ Kong/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.get('/api/saludo', (req, res) => {
+  res.json({ mensaje: "Hola desde API sin Docker 🚀" });
+});
+
+app.get('/api/test', (req, res) => {
+  res.json({ status: "OK" });
+});
+
+app.listen(3000, () => {
+  console.log("Servidor corriendo en http://localhost:3000");
+});
